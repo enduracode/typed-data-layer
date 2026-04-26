6fa3a9f620eb4d28ee21a013c509c25b1e36a1fe
William Gross <william@wgross.net>
2026-04-25 20:36:31 -0400

    Updated to .NET 4.8.1 and modern SqlClient

--- CommandRunner/DatabaseAbstraction/Databases/SqlServer.cs
+++ CommandRunner/DatabaseAbstraction/Databases/SqlServer.cs
@@ -1,6 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Text;
 using JetBrains.Annotations;
 using Microsoft.SqlServer.Management.Common;
